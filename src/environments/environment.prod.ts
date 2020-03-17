8fa8ca8086d7af90240676577c8af19c93eef25e
Fridolin Fridz <Fridolin@Fridolins-MacBook-Pro.local>
2020-03-17 06:42:03 +0200

    add firebase connection

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDPEFtWBW14vqp8PH-bGj5uWhy2SMkhY8E",
+    authDomain: "rt-calender.firebaseapp.com",
+    databaseURL: "https://rt-calender.firebaseio.com",
+    projectId: "rt-calender",
+    storageBucket: "rt-calender.appspot.com",
+    messagingSenderId: "913437347778",
+    appId: "1:913437347778:web:36d3239728eb7b91"
+  }
 };
