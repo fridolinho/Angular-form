62ffe8e397108dc55733dcd4bbe95b88e8b9e70d
niyonsaba fridolin <fridolin@gmail.com>
2020-04-01 18:42:02 +0200

    add upload

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -34,9 +34,8 @@ export class ContactFormComponent implements OnInit {
   matcher = new MyErrorStateMatcher();
   startDate = new Date(2020, 0, 1);
   localfiles = [];
-  files = [];
   input: HTMLElement;
-  attachement: {src: string, type: string }[] = [];
+  attachement: {src: string, name: string, type: string }[] = [];
 
 
   constructor(
@@ -56,8 +55,8 @@ export class ContactFormComponent implements OnInit {
       loyerNet: this.fb.control(null, [Validators.required]),
       charges: this.fb.control(null, [Validators.required]),
       referenceObjet: this.fb.control('', [Validators.required]),
-      parking: this.fb.control('', [Validators.required]),
-      chfCharges: this.fb.control(null, [Validators.required]),
+      parking: this.fb.control('', []),
+      chfCharges: this.fb.control(null, []),
       dateDemmenagementSouhaitee: this.fb.control('', [Validators.required]),
       dateDeVisite: this.fb.control('', [Validators.required]),
     });
@@ -70,17 +69,17 @@ export class ContactFormComponent implements OnInit {
       adresseLegale: this.fb.control(''),
       NPALocalite: this.fb.control(null, [Validators.required]),
       nationalite: this.fb.control('', [Validators.required]),
-      permis: this.fb.control('', [Validators.required]),
+      permis: this.fb.control('', []),
       lieuOrigine: this.fb.control('', [Validators.required]),
       dateDeNaissance: this.fb.control('', [Validators.required]),
       etatCivil: this.fb.control('', [Validators.required]),
-      telephoneFixe: this.fb.control(null, [Validators.required]),
-      telephoneProfessionel: this.fb.control('', [Validators.required]),
+      telephoneFixe: this.fb.control(null, []),
+      telephoneProfessionel: this.fb.control('', []),
       portable: this.fb.control(null, [Validators.required]),
       email: this.fb.control('', [Validators.required, Validators.email]),
       profession: this.fb.control('', [Validators.required]),
-      employeur: this.fb.control('', [Validators.required]),
-      depuis: this.fb.control('', [Validators.required]),
+      employeur: this.fb.control('', []),
+      depuis: this.fb.control('', []),
       revenueMensuel: this.fb.control(null, [Validators.required]),
       geranceActuelle: this.fb.control('', [Validators.required]),
       loyerActuelle: this.fb.control(null, [Validators.required]),
@@ -120,15 +119,15 @@ export class ContactFormComponent implements OnInit {
     this.fourthFormGroup = this.fb.group({
       logement: this.fb.control('', [Validators.required]),
       siOccupéParTitulaireOuCoTitulaire: this.fb.control('', [Validators.required]),
-      nomsDuTitulaireOuCoTitulaure: this.fb.control('', [Validators.required]),
+      nomsDuTitulaireOuCoTitulaure: this.fb.control('', []),
       occupantsAdultes: this.fb.control(null, [Validators.required]),
-      occupantsEnfants: this.fb.control(null, [Validators.required]),
-      animaux: this.fb.control('', [Validators.required]),
-      siAnimaux: this.fb.control('', [Validators.required]),
-      vehicules: this.fb.control('', [Validators.required]),
-      noPlaques: this.fb.control('', [Validators.required]),
-      siInstruments: this.fb.control('', [Validators.required]),
-      instruments: this.fb.control('', [Validators.required]),
+      occupantsEnfants: this.fb.control(null, []),
+      animaux: this.fb.control('', []),
+      siAnimaux: this.fb.control('', []),
+      vehicules: this.fb.control('', []),
+      noPlaques: this.fb.control('', []),
+      siInstruments: this.fb.control('', []),
+      instruments: this.fb.control('', []),
       siBailRésilié: this.fb.control('', [Validators.required]),
       résilièPar: this.fb.control('', [Validators.required]),
       motifDuDéménagement: this.fb.control('', [Validators.required]),
@@ -157,6 +156,11 @@ export class ContactFormComponent implements OnInit {
     return true;
   }
 
+  clickInput() {
+    this.input = document.getElementById('file_upload');
+    this.input.click();
+  }
+
   onSelectFile(event) {
     if (event.target.files && event.target.files[0]) {
       const reader = new FileReader();
@@ -168,7 +172,7 @@ export class ContactFormComponent implements OnInit {
         const task = this.storage.upload(filePath, file);
         task.then(async (snapshot) => {
           const src = await snapshot.ref.getDownloadURL();
-          this.files.push({src, name: file.name});
+          this.attachement.push({src, name: file.name, type: file.type});
         }).catch((error) => {
           console.error(error);
         });
@@ -179,14 +183,6 @@ export class ContactFormComponent implements OnInit {
   removeFile(file: any) {
     console.log(file);
     this.localfiles = this.localfiles.filter(item => item.name !== file);
-  }
-
-  clickInput() {
-    this.input = document.getElementById('file_upload');
-    this.input.click();
-  }
-
-  sendFilename(event: Event) {
-    console.log(event);
+    this.attachement = this.attachement.filter(item => item.name !== file);
   }
 }
