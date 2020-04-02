7f9cffa5b0be5b4223dc61d2e6bc205c18ac56b3
niyonsaba fridolin <fridolin@gmail.com>
2020-04-02 09:08:59 +0200

    upload files
     Author:    Fridolin Niyonsaba <fridolin@gmail.com>

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -32,8 +32,12 @@ export class ContactFormComponent implements OnInit {
     );
 
   matcher = new MyErrorStateMatcher();
-  images = [];
   startDate = new Date(2020, 0, 1);
+  localfiles = [];
+  filename: string
+  input: HTMLElement;
+  attachement: {filename: string, src: string, name: string, type: string }[] = [];
+
 
   constructor(
     private fb: FormBuilder,
@@ -52,8 +56,8 @@ export class ContactFormComponent implements OnInit {
       loyerNet: this.fb.control(null, [Validators.required]),
       charges: this.fb.control(null, [Validators.required]),
       referenceObjet: this.fb.control('', [Validators.required]),
-      parking: this.fb.control('', [Validators.required]),
-      chfCharges: this.fb.control(null, [Validators.required]),
+      parking: this.fb.control('', []),
+      chfCharges: this.fb.control(null, []),
       dateDemmenagementSouhaitee: this.fb.control('', [Validators.required]),
       dateDeVisite: this.fb.control('', [Validators.required]),
     });
@@ -66,17 +70,17 @@ export class ContactFormComponent implements OnInit {
       adresseLegale: this.fb.control(''),
       NPALocalite: this.fb.control(null, [Validators.required]),
       nationalite: this.fb.control('', [Validators.required]),
-      permis: this.fb.control('', [Validators.required]),
+      permis: this.fb.control('', []),
       lieuOrigine: this.fb.control('', [Validators.required]),
       dateDeNaissance: this.fb.control('', [Validators.required]),
       etatCivil: this.fb.control('', [Validators.required]),
-      telephoneFixe: this.fb.control(null, [Validators.required]),
-      telephoneProfessionel: this.fb.control('', [Validators.required]),
+      telephoneFixe: this.fb.control(null, []),
+      telephoneProfessionel: this.fb.control('', []),
       portable: this.fb.control(null, [Validators.required]),
       email: this.fb.control('', [Validators.required, Validators.email]),
       profession: this.fb.control('', [Validators.required]),
-      employeur: this.fb.control('', [Validators.required]),
-      depuis: this.fb.control('', [Validators.required]),
+      employeur: this.fb.control('', []),
+      depuis: this.fb.control('', []),
       revenueMensuel: this.fb.control(null, [Validators.required]),
       geranceActuelle: this.fb.control('', [Validators.required]),
       loyerActuelle: this.fb.control(null, [Validators.required]),
@@ -86,45 +90,45 @@ export class ContactFormComponent implements OnInit {
     });
 
     this.thirdFormGroup = this.fb.group({
-      coTitulaire: this.fb.control('', [Validators.required]),
-      nom: this.fb.control('', [Validators.required]),
-      prenom: this.fb.control('', [Validators.required]),
-      adresseActuelle: this.fb.control('', [Validators.required]),
+      coTitulaire: this.fb.control('', []),
+      nom: this.fb.control('', []),
+      prenom: this.fb.control('', []),
+      adresseActuelle: this.fb.control('', []),
       adresseSiDifferente: this.fb.control(''),
-      NPALocalite: this.fb.control(null, [Validators.required]),
-      nationalite: this.fb.control('', [Validators.required]),
-      permis: this.fb.control('', [Validators.required]),
-      lieuDorigine: this.fb.control('', [Validators.required]),
-      dateDeNaissance: this.fb.control('', [Validators.required]),
-      etatCivil: this.fb.control('', [Validators.required]),
-      telephoneFixe: this.fb.control(null, [Validators.required]),
-      telephoneProfessionel: this.fb.control(null, [Validators.required]),
-      portable: this.fb.control(null, [Validators.required]),
-      email: this.fb.control('', [Validators.required, Validators.email]),
-      profession: this.fb.control('', [Validators.required]),
-      employeur: this.fb.control('', [Validators.required]),
-      depuis: this.fb.control('', [Validators.required]),
-      revenueMensuel: this.fb.control(null, [Validators.required]),
-      geranceActuelle: this.fb.control('', [Validators.required]),
-      loyerActuelle: this.fb.control(null, [Validators.required]),
-      charges: this.fb.control(null, [Validators.required]),
-      poursuites: this.fb.control('', [Validators.required]),
-      protection: this.fb.control('', [Validators.required]),
-      siTuteur: this.fb.control('', [Validators.required]),
+      NPALocalite: this.fb.control(null, []),
+      nationalite: this.fb.control('', []),
+      permis: this.fb.control('', []),
+      lieuDorigine: this.fb.control('', []),
+      dateDeNaissance: this.fb.control('', []),
+      etatCivil: this.fb.control('', []),
+      telephoneFixe: this.fb.control(null, []),
+      telephoneProfessionel: this.fb.control(null, []),
+      portable: this.fb.control(null, []),
+      email: this.fb.control('', [Validators.email]),
+      profession: this.fb.control('', []),
+      employeur: this.fb.control('', []),
+      depuis: this.fb.control('', []),
+      revenueMensuel: this.fb.control(null, []),
+      geranceActuelle: this.fb.control('', []),
+      loyerActuelle: this.fb.control(null, []),
+      charges: this.fb.control(null, []),
+      poursuites: this.fb.control('', []),
+      protection: this.fb.control('', []),
+      siTuteur: this.fb.control('', []),
     });
 
     this.fourthFormGroup = this.fb.group({
       logement: this.fb.control('', [Validators.required]),
       siOccupéParTitulaireOuCoTitulaire: this.fb.control('', [Validators.required]),
-      nomsDuTitulaireOuCoTitulaure: this.fb.control('', [Validators.required]),
+      nomsDuTitulaireOuCoTitulaure: this.fb.control('', []),
       occupantsAdultes: this.fb.control(null, [Validators.required]),
-      occupantsEnfants: this.fb.control(null, [Validators.required]),
-      animaux: this.fb.control('', [Validators.required]),
-      siAnimaux: this.fb.control('', [Validators.required]),
-      vehicules: this.fb.control('', [Validators.required]),
-      noPlaques: this.fb.control('', [Validators.required]),
-      siInstruments: this.fb.control('', [Validators.required]),
-      instruments: this.fb.control('', [Validators.required]),
+      occupantsEnfants: this.fb.control(null, []),
+      animaux: this.fb.control('', []),
+      siAnimaux: this.fb.control('', []),
+      vehicules: this.fb.control('', []),
+      noPlaques: this.fb.control('', []),
+      siInstruments: this.fb.control('', []),
+      instruments: this.fb.control('', []),
       siBailRésilié: this.fb.control('', [Validators.required]),
       résilièPar: this.fb.control('', [Validators.required]),
       motifDuDéménagement: this.fb.control('', [Validators.required]),
@@ -137,7 +141,8 @@ export class ContactFormComponent implements OnInit {
       immobilier: this.firstFormGroup.value,
       locataire: this.secondFormGroup.value,
       coTitulaire: this.thirdFormGroup.value,
-      plusDetails: this.fourthFormGroup.value
+      plusDetails: this.fourthFormGroup.value,
+      attachement: this.attachement
     };
 
     console.log(formData);
@@ -152,18 +157,24 @@ export class ContactFormComponent implements OnInit {
     return true;
   }
 
+  clickInput() {
+    this.input = document.getElementById('file_upload');
+    this.input.click();
+  }
+
   onSelectFile(event) {
     if (event.target.files && event.target.files[0]) {
       const reader = new FileReader();
       const file = event.target.files[0];
-      const filePath = `order-image-${Date.now()}`;
-
+      const filePath = `file-${Date.now()}`;
+      const filename = this.filename;
       reader.readAsDataURL(event.target.files[0]); // read file as data url
       reader.onload = (event) => { // called once readAsDataURL is completed
-        this.images.push({src: event.target.result});
+        this.localfiles.push({filename, src: event.target.result, name: file.name, type: file.type});
         const task = this.storage.upload(filePath, file);
         task.then(async (snapshot) => {
           const src = await snapshot.ref.getDownloadURL();
+          this.attachement.push({filename, src, name: file.name, type: file.type});
         }).catch((error) => {
           console.error(error);
         });
@@ -171,7 +182,9 @@ export class ContactFormComponent implements OnInit {
     }
   }
 
-  removeImage(image: any) {
-    this.images = this.images.filter(item => item.src !== image.src);
+  removeFile(file: any) {
+    console.log(file);
+    this.localfiles = this.localfiles.filter(item => item.name !== file);
+    this.attachement = this.attachement.filter(item => item.name !== file);
   }
 }
--- src/app/components/view-users/view-users.component.ts
+++ src/app/components/view-users/view-users.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireStorage } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-view-users',
@@ -8,38 +7,8 @@ import { AngularFireStorage } from '@angular/fire/storage';
 })
 export class ViewUsersComponent implements OnInit {
 
-  images = [];
-  constructor(
-    private storage: AngularFireStorage
-  ) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
-
-  onSelectFile(event) {
-    if (event.target.files && event.target.files[0]) {
-      const reader = new FileReader();
-      const file = event.target.files[0];
-      const filePath = `image-${Date.now()}`;
-      reader.readAsDataURL(event.target.files[0]); // read file as data url
-      reader.onload = (event) => { // called once readAsDataURL is completed
-        this.images.push({src: event.target.result});
-        const task = this.storage.upload(filePath, file);
-        task.then(async (snapshot) => {
-          const src = await snapshot.ref.getDownloadURL();
-        }).catch((error) => {
-          console.error(error);
-        });
-      };
-    }
-  }
-
-  removeImage(image: any) {
-    this.images = this.images.filter(item => item.src !== image.src);
-  }
-
-  uploadImg() {
-    console.log(this.images);
-  }
-
 }
